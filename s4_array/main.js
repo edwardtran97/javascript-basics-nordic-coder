7ba377dca47e6b6438b524b510620667a0e9db9c
edwardtran97 <thuan.tranquoc0902@gmail.com>
2020-09-19 00:38:37 +0700

    Update s4_array (continue)

--- s4_array/main.js
+++ s4_array/main.js
@@ -185,7 +185,7 @@ console.log(findNumber([0, 1, 2, 3, 4]));
 
 
 
-// Kỹ thuật đặt lính canh
+// Kỹ thuật đặt lính canh (tìm một phần tử thỏa điều kiện nào đó)
 const findMax = (numberList) => {
     // return null in case of empty list
     if (!Array.isArray(numberList) || numberList.length === 0) return null;
@@ -228,7 +228,58 @@ console.log(isPrime(10));
 console.log(isPrime(9));
 
 
+
+
+
+
+// Array of Word Lengths
+// https://edabit.com/challenge/B9yPEStmax7Jxb9Dj
+
+const wordLengths = (arr) => {
+	if (!Array.isArray(arr) || arr.length === 0) return false;
+	
+	let newArr = [];
+	for (i = 0; i < arr.length; i += 1) {
+		newArr.push(arr[i].length);
+	}
+	return newArr;
+}
+console.log(wordLengths(['Thuan', 'Tran']));
+console.log(wordLengths(['hello', 'world']));
+
+
+// Use map method
+const wordLengths = (arr) => {
+    return arr.map(x => x.length);
+}
+console.log(wordLengths(['Thuan', 'Tran']));
+console.log(wordLengths(['hello', 'world']));
+
+
+
+
 // Tìm số nguyên tố lớn nhất trong mảng
+
+
+// Kiểm tra có phải mảng tăng dần hay không?
+const checkIncreaseArray = (numberList) => {
+    if (!Array.isArray(numberList)) return false;
+    if (numberList.length < 2) return true;
+
+    for (let i = 0; i < numberList.length - 1; i++) {
+        if (numberList[i + 1] < numberList[i]) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+console.log(checkIncreaseArray([1, 2, 3, 4, 5]));
+console.log(checkIncreaseArray([1, 2, 4, 3, 5]));
+console.log(checkIncreaseArray([0, 2, 1, 0, 5]));
+
+
+
 
 
 
@@ -254,29 +305,180 @@ console.log(isPrime(9));
 
 // Add new items at the end
 const numbers = [1, 2, 3];
+numbers.push(4);
 
 console.log(numbers);
 
 // Remove items at the end
 const numbers = [1, 2, 3];
-
+const lastNumber = numbers.pop();
 console.log(numbers, lastNumber);
 
 
 // Add new items at the beginning
 const numbers = [1, 2, 3];
+numbers.unshift(0);
 
 console.log(numbers);
 
-// Add new items at the beginning
+// Remove new items at the beginning
 const numbers = [1, 2, 3];
-
+const firstNumber = numbers.shift();
 console.log(numbers, firstNumber);
 
 
 
 // LAB: Sum of two arrays. [1, 2, 3] + [2, 4, 6] = [3, 6, 9];
 // LAB: Generate an array of random numbers
+
+
+
+// Tìm học sinh có điểm trung bình môn cao nhất
+// Điểm trung bình: toán * 2 + lý + hóa / 4
+
+const findStudent = (studentList) => {
+    if (!Array.isArray(studentList) || studentList.length === 0) return null;
+
+    let maxGrade = (studentList[0].math * 2 + studentList[0].physics + studentList[0].chemistry) / 4;
+    let bestStudent = studentList[0].name;
+
+    for (let i = 0; i < studentList.length; i++) {
+        
+        if (studentList[i].math * 2 + studentList[i].physics + studentList[i].chemistry > maxGrade) {
+            maxGrade = (studentList[i].math * 2 + studentList[i].physics + studentList[i].chemistry) / 4;
+            bestStudent = studentList[i].name;
+        }
+    }
+    return `${maxGrade}, ${bestStudent}`;
+}
+
+const studentList = [
+    {
+        id: 1,
+        name: 'Hau',
+        math: 5,
+        physics: 6,
+        chemistry: 4,
+    },
+    {
+        id: 2,
+        name: 'Hien',
+        math: 10,
+        physics: 9,
+        chemistry: 5,
+    },
+];
+
+console.log(findStudent(studentList));
+
+
+
+// Viết function kiểm tra trong array có 2 số nào mà tổng của nó bằng tham số thứ 2 hay không. Nếu có thì return 2 số đó.
+
+const findTwoNumber = (arr, number) => {
+    if (!Array.isArray(arr) || arr.length === 0) return false;
+
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i += 1) {
+        for (let j = i + 1; j < arr.length; j += 1) {
+            if (arr[i] + arr[j] === number) newArr.push(arr[i], arr[j]);
+        }
+    }
+    return newArr;
+}
+
+console.log(findTwoNumber([1, 2, 3], 5));
+console.log(findTwoNumber([1, 2, 3, 4, 5, 6], 8));
+
+
+
+// Kỹ thuật đếm 
+// Đếm số lẻ âm và số chẵn dương
+
+const countPositiveNumber = (numberList) => {
+    if(!Array.isArray(numberList) || numberList.length === 0) return 0;
+
+    let count = 0;
+
+    for (let i = 0; i < numberList.length; i += 1) {
+        if (numberList[i] < 0 && numberList[i] % 2 === -1 || numberList[i] > 0 && numberList[i] % 2 === 0)  {
+            count += 1;
+        }
+    }
+    return count;
+}
+console.log(countPositiveNumber([-3, -2, -1, 1, 2, 4]));
+console.log(countPositiveNumber([-4, -2, -1, 2, 3, 5, 6]));
+
+
+
+
+const sumNumberAtEvenPosition = (numberList) => {
+    if (!Array.isArray(numberList) || numberList.length === 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 0; i < numberList.length; i += 2) {
+        sum += number[i];
+    }
+    return sum;
+}
+
+
+
+
+// Callback
+// imperative programming
+// declarative programming
+
+
+// Liệt kê các số dương và số chẵn trong mảng
+const findAllEvenPositiveNumber = (numberList) => {
+    if (!Array.isArray(numberList) || numberList.length === 0) return [];
+
+    const newArr = [];
+    for (let i = 0; i < numberList.length; i += 1) {
+        if (numberList[i] > 0 && numberList[i] % 2 === 0) {
+            newArr.push(numberList[i]);
+        }
+    }
+    return newArr;
+}
+console.log(findAllEvenPositiveNumber([1, 2, 3, 4, 5, 6]));
+console.log(findAllEvenPositiveNumber([-2, 0, 3, 4, 5, 6]));
+
+
+
+
+// Callback: truyền vào function một tham số chính là một function
+
+const isEven = (number) => number % 2 === 0;
+
+console.log(isEven);
+
+const findAllEvenNumber = (numberList, isEvenFunction) => {
+    const evenNumberList = [];
+
+    for (let i = 0; i < numberList.length; i++) {
+        const number = numberList[i];
+
+        if (isEvenFunction(number)) evenNumberList.push(number);
+        
+    }
+    return evenNumberList;
+}
+
+console.log(
+    findAllEvenNumber(
+        [1, 2, 3, 4],
+        isEven
+    )
+);
+
+
+// Use filter() method
+ const evenNumberList = numberList.filter(isEven);
 
 
 /**
